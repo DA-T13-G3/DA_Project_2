3dec3fce99d0529a99a7890b63ea0d8d927993b6
Gustavo Bastos <gustpcb1824@gmail.com>
2026-05-13 12:38:03 +0100

    initial version of graphColoringBasic, not tested

--- src/graphColoring.h
+++ src/graphColoring.h
@@ -69,6 +69,85 @@ int graphColoring(Graph<T>* g) {
 }
 
 
+template<typename T>
+int graphColoringBasic(Graph<T>* g,unsigned int N) {
+    bool graph_empty=false;
+    std::vector<Vertex<T>*>stack;
+    for (auto v:g->getVertexSet()) {
+        v.setVisited(false);
+        v->setIndegree(v->getAdj().size());
+    }
+    int n_nodes=g->getVertexSet().size();
+    bool all_remaining_nodes_degree_eq_gt_N=true;
+    while (n_nodes>0) {
+        for (auto v:g->getVertexSet()) {
+            if (!v->isVisited()) {
+                if (v->getIndegree()<N) {
+                    n_nodes--;
+                    stack.push_back(v);
+                    v->setVisited(true);
+                    for (auto e:v->getAdj()) {
+                        Vertex<T>* w=e->getDest();
+                        w->setIndegree(w->getIndegree()-1);
+                    }
+                }
+            }
+        }
+        all_remaining_nodes_degree_eq_gt_N=true;
+        for (auto v:g->getVertexSet()) {
+            if (!v->isVisited()) {
+                if (v->getIndegree()<N) {
+                    all_remaining_nodes_degree_eq_gt_N=false;
+                }
+            }
+        }
+        if (n_nodes!=0 && all_remaining_nodes_degree_eq_gt_N) {
+            return (int)(N+1);
+        }
+    }
+
+    for (auto &v:g->getVertexSet()) {
+        v->setVisited(false);
+        v->setNum(-1);
+    }
+
+    for (auto &v:g->getVertexSet()) {
+        v->setNum(-1);
+    }
+
+
+    unsigned int biggestColor=0;
+    std::vector<bool>usedColors(N,0);
+
+    while (!stack.empty()) {
+        Vertex<T>* v=stack.back();
+        stack.pop_back();
+        for (auto e:v->getAdj()) {
+            Vertex<T>*w=e->getDest();
+            if (w->getNum()!=-1) {
+                usedColors[w->getNum()]=true;
+            }
+        }
+
+        unsigned int i=0;
+        for (bool b:usedColors) {
+            if (!b) {
+                v->setNum(i);
+                biggestColor=std::max(i,biggestColor);
+                break;
+            }
+        }
+        for ( unsigned int i=0;i<N;i++) {
+            usedColors[i] = false;
+        }
+    }
+
+    return biggestColor;
+
+
+}
+
+
 /*
 template<typename T>
 int graphColoring(Graph<T>* g) {
