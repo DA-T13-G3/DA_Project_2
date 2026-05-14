439ca71b78c41b016780ebebdb39bd4bc6d29ea3
Tmfzes <tomasmorfig@gmail.com>
2026-05-14 23:57:37 +0100

    Added documentation for the create graph algorithm.

--- src/createGraph.h
+++ src/createGraph.h
@@ -2,6 +2,17 @@
 #define DA_PROJECT_2_CREATEGRAPH_H
 #include "dataStructs.h"
 #include <algorithm>
-
+/**
+ * @brief Builds the graph from the set of webs.
+ *
+ * This function constructs a graph where each web corresponds to a vertex.
+ * For every pair of webs, an interference edge is added if their live ranges
+ * overlap according to 'interfere()'. The resulting graph is the
+ * input for the register allocation algorithms.
+ *
+ * @param info  Global parameters containing the list of webs to insert in the graph.
+ *
+ * @return A Graph<web> object representing the full interference graph.
+ */
 Graph<web>create(params info);
 #endif
