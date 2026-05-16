cf2fe291f759fc16e9e7fa3bb9cdf23f3e077246
David Ferreira <dab.ferreira75@gmail.com>
2026-05-17 00:09:27 +0100

    Added documentation for the methods at spilling.cpp and .h

--- src/spilling.h
+++ src/spilling.h
@@ -9,7 +9,29 @@
 #include <limits>
 #include <functional>
 
-
+/**
+ * @brief Builds a result struct while spilling a certain amount of webs into memory.
+ * 
+ * This method extracts the graph's vertices, isolates a strategic subset of size 'X' containing 
+ * the highest-degree candidates, and runs a binary search over the range 
+ * of possible spill numbers. The search uses attempt() to find the smallest number of spills needed.
+ * Finally, it invokes finalSpill() to commit the highest-efficiency combination and calls 
+ * build() to construct the final allocation report.
+ * 
+ * @details Details on the Heuristics Applied
+ * To make this function run in a practical timeframe, it tries to spill only a certain fixed
+ * number of combinations of webs, which significantly decreases its time complexity. It starts by trying
+ * to spill the nodes with the biggest degrees, as these are the ones most likely to increase the
+ * number of necessary registers, and then moves on to spilling nodes with lower degrees if that
+ * doesn't work.
+ * This compromise trades unnecessary precision for a very significant increase in speed, which is why
+ * it was chosen.
+ * 
+ * @param g Pointer to the interference graph to process.
+ * @param info Struct holding algorithm settings, limits, and user choices.
+ * @param coloring Function that will execute the coloring of the graph.
+ * @return A result structure stating whether allocation succeeded and mapping variables to registers.
+ */
 struct result spill(Graph<web>* g, params info, function<int(Graph<web>* g, int)> coloring);
 
 #endif
